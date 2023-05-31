68b83e3d91c1b6a3c6da20c25852d1fd0c85a29c
talish786 <jam.talish@gmail.com>
2023-05-31 23:43:11 +0500

    Frontend Home And PDP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
-    <div className="App">
-      <h1>Ecommerce</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} exact />
+              <Route path="/product/:id" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
