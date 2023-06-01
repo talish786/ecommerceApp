bb48bff1a2faa8771a4085aef2278bceea8ace13
talish786 <jam.talish@gmail.com>
2023-06-01 23:07:25 +0500

    connected with mongobd

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import products from "./data/products.js";
+import colors from "colors";
 import dotenv from "dotenv";
-
+import connectDB from "./config/db.js";
 dotenv.config();
 
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
@@ -23,5 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`App Is running in ${process.env.NODE_ENV} on ${PORT} port`)
+  console.log(
+    `App Is running in ${process.env.NODE_ENV} on ${PORT} port`.yellow.bold
+  )
 );
